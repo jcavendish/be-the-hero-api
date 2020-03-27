590ec126f2c2409f0cbdb3b75ce5b11a6df9bbd9
Joao Paulo Cavendish <joaopaulocavendish@Joaos-MacBook-Pro.local>
2020-03-27 15:24:44 -0300

    validate ong in profile controller

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -3,6 +3,16 @@ const connection = require("../database/connection");
 module.exports = {
   async index(request, response) {
     const ong_id = request.headers.authorization;
+    const ong = await connection("ongs")
+        .select("id")
+        .where("id", ong_id)
+        .first();
+    if (!ong) {
+      return response
+          .status(403)
+          .send("You cannot access this resource");
+    }
+
     const incidents = await connection("incidents")
         .join("ongs", "incidents.ong_id", "ongs.id")
         .where("incidents.ong_id", ong_id)
